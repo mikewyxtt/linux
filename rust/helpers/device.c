e06a4f3df91371cd5a6d09ca60b318308eee7e49
Asahi Lina <lina@asahilina.net>
2022-09-07 17:25:18 +0900

    rust: kernel: platform: Add Device.set_dma_masks()
    Allows drivers to configure the DMA masks for a device. Implemented
    here, not in device, because it requires a mutable platform device
    reference this way (device::Device is a safely clonable reference).
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/helpers/device.c
+++ rust/helpers/device.c
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/device.h>
-#include <linux/export.h>
+#include <linux/dma-mapping.h>
 
 void *rust_helper_dev_get_drvdata(struct device *dev)
 {
 	return dev_get_drvdata(dev);
 }
-EXPORT_SYMBOL_GPL(rust_helper_dev_get_drvdata);
 
 const char *rust_helper_dev_name(const struct device *dev)
 {
 	return dev_name(dev);
 }
-EXPORT_SYMBOL_GPL(rust_helper_dev_name);
+
+int rust_helper_dma_set_mask_and_coherent(struct device *dev, u64 mask)
+{
+	return dma_set_mask_and_coherent(dev, mask);
+}
