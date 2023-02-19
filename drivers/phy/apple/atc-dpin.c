37c10c3a35c30ba2591a199390253349d7529452
Sven Peter <sven@svenpeter.dev>
2023-02-19 16:40:43 +0100

    phy: apple: Add DPIN
    Signed-off-by: Sven Peter <sven@svenpeter.dev>

---
+++ drivers/phy/apple/atc-dpin.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+/*
+ * Apple Silicon Type-C PHY USB4 DisplayPort Protocol Adapter driver
+ *
+ * Copyright (C) Asahi Linux Contributors
+ * Author: Sven Peter <sven@svenpeter.dev>
+ */
+
+#include <linux/module.h>
+
+MODULE_DESCRIPTION("Apple Silicon Type-C PHY USB4 DisplayPort Protocol Adapter");
+MODULE_AUTHOR("Sven Peter <sven@svenpeter.dev>");
+MODULE_LICENSE("GPL v2");
