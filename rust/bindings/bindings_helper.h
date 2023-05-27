7391479537239c8c7f0e23a5009fd85285f05d60
Asahi Lina <lina@asahilina.net>
2023-05-27 21:21:06 +0900

    rust: sync: Add LockdepMap abstraction
    This allows Rust code to explicitly integrate types with lockdep.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -13,6 +13,7 @@
 #include <linux/jiffies.h>
 #include <linux/mdio.h>
 #include <linux/phy.h>
+#include <linux/lockdep.h>
 #include <linux/refcount.h>
 #include <linux/siphash.h>
 #include <linux/sched.h>
