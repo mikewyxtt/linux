89d0dde8fe88bec7e0e9971c458dd1648ceca93e
Janne Grunau <j@jannau.net>
2024-08-21 21:36:34 +0200

    squash! power: supply: macsmc_power: Add CHLS charge thresholds
    Prefer CHWA over CHLS since not all SMC firmware versions implement
    CHLS in a compatible way.
    
    Signed-off-by: Janne Grunau <j@jannau.net>
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/power/supply/macsmc_power.c
+++ drivers/power/supply/macsmc_power.c
@@ -805,10 +805,11 @@ static int macsmc_power_probe(struct platform_device *pdev)
 	apple_smc_write_u8(power->smc, SMC_KEY(CH0K), 0);
 	apple_smc_write_u8(power->smc, SMC_KEY(CH0B), 0);
 
-	if (apple_smc_read_u16(power->smc, SMC_KEY(CHLS), &vu16) >= 0) {
-		power->has_chls = true;
-	} else if (apple_smc_read_flag(power->smc, SMC_KEY(CHWA)) >= 0) {
+	/* prefer CHWA as some SMC firmwares have an incompatible CHLS */
+	if (apple_smc_read_flag(power->smc, SMC_KEY(CHWA)) >= 0) {
 		power->has_chwa = true;
+	} else if (apple_smc_read_u16(power->smc, SMC_KEY(CHLS), &vu16) >= 0) {
+		power->has_chls = true;
 	} else {
 		/* Remove the last 2 properties that control the charge threshold */
 		power->batt_desc.num_properties -= 2;
